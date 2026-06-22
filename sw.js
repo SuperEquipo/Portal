d0df3df85f5a8ffe6dd1808de3eeee19d62375e9
SuperEquipo <jorgeentrenadoria@gmail.com>
2026-06-22 19:57:13 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado');
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => {
-        return new Response('Sin conexión a internet.');
-    }));
-});
